85851be8626fc47f3121dcd42ef158e6dbeb345b
Chris Houghton <chris.houghton@qubitdigital.com>
2013-10-29 09:27:09 +0000

    initial code release

--- README.md
+++ README.md
@@ -1,2 +1,158 @@
-universal-variable-php-library
-==============================
+# Universal Variable PHP Library
+
+A high level library for creating Universal Variable with PHP.
+
+
+## Introduction
+
+Universal Variable (UV) always requires some server-side development work to instantiate on each page of websites. At [Qubit](http://qubitproducts.com) we've created a PHP library to assist with this, thus requiring a little less 
+intimate knowledge of client-side JavaScript.
+
+This library is not prescriptive - it can be used to create the standard UV data layer, as well as extending it for more advanced and custom functionality.
+
+Requires PHP version 5.2.0 or greater.
+
+
+## Example
+
+Create a page object within the UV and return json.
+
+```php
+$uv = new BuildUV();
+$uv->get("page")->set("category", "Home");
+$uv->get("page")->set("subcategory", "Mens - Shoes");
+
+print $uv->toJSON()
+=> {"page":{"category":"Home","subcategory":"Mens - Shoes"},"user":{},"events":[]}
+```
+
+
+## Installation
+
+Download the library directly from Github:
+
+```
+git clone tbc@tbc.com
+```
+
+Include the `uv_library.php` file in any models/templates where you wish to instantiate UV.
+
+
+
+## API
+
+### Instantiation
+
+Before you can run any methods, create the UV class:
+
+```php
+$uv = new BuildUV();
+```
+
+This automatically instantiates the standard specification object variables:
+
+* page
+* user
+* product
+* listing
+* basket
+* transaction
+* events (array)
+
+
+### get
+
+Retreive a varaible within the UV. Works with any pre-declared variables.
+
+```php
+$uv->get("page")
+=> UVObject Object ( )
+```
+
+The `get` method is usually only used to retreive a variable prior to setting one of its children.
+
+
+### set
+
+Set variables within the `product` object:
+
+```php
+$uv->get("product")->set("id", "12324214");
+$uv->get("product")->set("name", "Sparkly Shoes");
+$uv->get("product")->set("unit_price", 15);
+```
+
+
+### add 
+
+Add a variable to an array. The following are the standard arrays found in UV:
+
+* `listing.items`
+* `basket.line_items`
+* `transaction.line_items`
+* `events`
+
+```php
+
+// Add event object to the events array
+$uv->get("events")->add(array(
+  "category": "checkout",
+  "action": "step1"
+));
+
+// Add a line item object to the line_items array
+$uv->get("basket")->get("line_items")->add(array(
+  "product" => array(
+    "id" => "12342332"
+  ),
+  "subtotal" => 10,
+  "quantity" => 1
+));
+```
+
+### toJSON
+
+Outputs the UV object as json. Uses `json_encode`.
+
+```php
+print $uv->toJSON();
+=> {"page":{"category":"Home","subcategory":"Mens - Shoes"},"user":{},"events":[]}
+```
+
+### toHTML
+
+Outputs the UV object as json, declared as `window.universal_variable`, within `<script>` tags:
+
+```php
+print $uv->toHTML();
+=>  <!-- Qubit Universal Variable data layer -->
+    <script>
+      window.universal_variable = {"page":{"category":"Home","subcategory":"Mens - Shoes"},"user":{},"events":[]};
+    </script>
+```
+
+
+### Custom variables
+
+If you'd like to add additional custom variables to the UV, you can do so by instantiating a new `UVObject` or `UVArray` class, and then run the methods as normal:
+
+```php
+// Object
+$uv->my_custom = new UVObject();
+$uv->get("my_custom")->set("id", "54321");
+
+// Array
+$uv->clicks = new UVArray();
+
+// Add a string
+$uv->add("click_button"); 
+
+// Add an object
+$uv->add(array(
+  "category" => "signup",
+  "action" => "button_click"
+));
+```
+
+
+
---
+++ uv-library.php
@@ -0,0 +1,133 @@
+<?php
+
+  class UVArray extends UVObject {
+
+    public function add($object) {
+      $count = count(get_object_vars($this));
+      $this->$count = new UVObject($object);
+    }
+
+    public function set($key, $value) {
+      if (is_numeric($key)) {
+        $this->$key = $value;
+      }
+    }
+
+  }
+
+  class UVObject {
+
+    public function __construct($attributes=array()) {
+      if (!empty($attributes)) {
+        foreach ($attributes as $key => $value) {
+          $this->set($key, $value);
+        }
+      }
+      $this->convertNestedObjects();
+    }
+
+    public function set($key, $value) {
+      $this->$key = $value;
+    }
+
+    public function get($index) {
+      return $this->$index;
+    }
+
+    public function toArray() {
+      $obj = array();
+      $vars = get_object_vars($this);
+      foreach ($vars as $key => $value) {
+        if (is_object($value) && method_exists($value, "toArray")) {
+          $obj[$key] = $value->toArray();
+        } else {
+          $obj[$key] = $value;
+        }
+      }
+      if (count($obj) == 0 && !($this instanceof UVArray)) {
+        return new stdClass();
+      }
+      return $obj;
+    }
+
+    public function isEmpty() {
+      $vars = get_object_vars($this);
+      if (empty($vars)) {
+        return true;
+      } else if (count($vars) === 1 && isset($vars["line_items"]) && count(get_object_vars($vars["line_items"])) == 0) {
+        return true;
+      } else if (count($vars) === 1 && isset($vars["items"]) && count(get_object_vars($vars["items"])) == 0) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    private function convertNestedObjects() {
+      $vars = get_object_vars($this);
+      foreach($vars as $key => $value) {
+        if (is_array($value) && $this->isAssociative($value)) {
+          $this->$key = new UVObject($value);
+        } else if (is_array($value)) {
+          $this->$key = new UVArray($value);
+        }
+      }
+    }
+
+    private function isAssociative($arr) {
+      return array_keys($arr) !== range(0, count($arr) - 1);
+    }
+
+  }
+
+
+  class BuildUV extends UVObject {
+
+    public function __construct() {
+      $this->set("page", new UVObject());
+      $this->set("user", new UVObject());
+      $this->set("product", new UVObject());
+      $this->set("listing", new UVObject());
+      $this->get("listing")->set("items", new UVArray());
+      $this->set("transaction", new UVObject());
+      $this->get("transaction")->set("line_items", new UVArray());
+      $this->set("basket", new UVObject());
+      $this->get("basket")->set("line_items", new UVArray());
+      $this->set("events", new UVArray());
+    }
+
+    public function toJSON() {
+      $uvOutput = array();
+
+      $uvOutput["page"] = $this->page->toArray();
+      $uvOutput["user"] = $this->user->toArray();
+      $uvOutput["events"] = $this->events->toArray();
+
+      $vars = get_object_vars($this);
+      foreach ($vars as $key => $value) {
+        if (isset($uvOutput[$key])) {
+          continue;
+        }
+        if (method_exists($value, "isEmpty") && !$value->isEmpty()) {
+          if (method_exists($value, "toArray")) {
+            $uvOutput[$key] = $value->toArray();
+          } else {
+            $uvOutput[$key] = $value;
+          }
+        }
+      }
+      return json_encode($uvOutput);
+    }
+
+    public function toHTML() {
+      return "\n
+      <!-- Qubit Universal Variable data layer -->
+      <script>
+        window.universal_variable = " . $this->toJSON() . ";
+      </script>
+      \n";
+    }
+
+  }
+
+?>
